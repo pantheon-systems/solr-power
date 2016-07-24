849f88cb0afd7f752f87f9aaffd6337435807df3
ataylorme <andrew@ataylor.me>
2016-07-24 10:52:21 -0500

    Remove duplicate trailing slash on includes

--- solr-power.php
+++ solr-power.php
@@ -112,7 +112,7 @@ if ( version_compare( PHP_VERSION, '5.4', '<' ) ) {
 	add_action( 'admin_notices', 'solr_power_env_variables_admin_notice' );
 	add_action( 'admin_init', 'solr_power__deactivate' );
 } else {
-	define( 'SOLR_POWER_PATH', plugin_dir_path( __FILE__ ) . '/' );
+	define( 'SOLR_POWER_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 	define( 'SOLR_POWER_URL', plugin_dir_url( __FILE__ ) );
 
 	require_once( SOLR_POWER_PATH . '/vendor/autoload.php' );
