26a45d6810ad2b7898f987a0beaf1e38a8edc0cd
ataylorme <andrew@ataylor.me>
2016-07-24 22:22:52 -0500

    xargs instead of exec

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -46,7 +46,7 @@ rm -rf $PREPARE_DIR/wp-content/plugins/solr-power/.git
 ###
 cd $PREPARE_DIR
 git rm -rf --cached wp-content/plugins/solr-power/vendor
-find $PREPARE_DIR/wp-content/plugins/solr-power/vendor -type d -name ".git" -exec rm -rf "{}" \;
+find $PREPARE_DIR/wp-content/plugins/solr-power/vendor -type d -name '.git' | xargs rm -rf
 git add -A wp-content
 git config user.email "solr-power@getpantheon.com"
 git config user.name "Pantheon"
