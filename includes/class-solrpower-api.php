2975a26c88d234750e13289ce4d7b2b83edc067d
Chris Wiegman <chris.wiegman@10up.com>
2016-08-10 10:38:31 -0400

    Add get_server method
    Adds a method to pass server configuration information where needed.

--- includes/class-solrpower-api.php
+++ includes/class-solrpower-api.php
@@ -461,4 +461,24 @@ class SolrPower_Api {
 
 		}
 	}
+
+	/**
+	 * Returns an array of server information.
+	 *
+	 * @since 1.0
+	 *
+	 * @return array Array of server connection information.
+	 */
+	public function get_server() {
+
+		$ping = SolrPower_Api::get_instance()->ping_server();
+
+		return array(
+			'ping_status' => $ping ? 'successful' : 'failed',
+			'ip_address'  => getenv( 'PANTHEON_INDEX_HOST' ),
+			'port'        => getenv( 'PANTHEON_INDEX_PORT' ),
+			'path'        => SolrPower_Api::get_instance()->compute_path(),
+		);
+
+	}
 }
