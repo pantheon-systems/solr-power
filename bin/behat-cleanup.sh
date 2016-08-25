936859e070ac959b706eb28303a797bb6f3e7bdd
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-25 09:40:18 -0700

    Use a better unauthenticated check
    This way, the script can be run when `terminus` is already
    authenticated, but `TERMINUS_TOKEN` isn't present

--- bin/behat-cleanup.sh
+++ bin/behat-cleanup.sh
@@ -4,12 +4,18 @@
 # Delete the Pantheon site environment after the Behat test suite has run.
 ###
 
-set -ex
+terminus auth whoami > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Terminus unauthenticated; assuming unauthenticated build"
+	exit 0
+fi
 
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
 	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
 	exit 1
 fi
+
+set -ex
 
 ###
 # Delete the environment used for this test run.
