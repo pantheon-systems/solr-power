ec7193af551b6be6a27c354cec275bc39bfb7a24
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-25 10:16:33 -0700

    Use a better unauthenticated check (#130)
    This way, the script can be run when `terminus` is already
    authenticated, but `TERMINUS_TOKEN` isn't present

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -6,12 +6,23 @@
 # such that it can be run a second time if a step fails.
 ###
 
-set -ex
+terminus auth whoami > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Terminus unauthenticated; assuming unauthenticated build"
+	exit 0
+fi
 
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
 	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
 	exit 1
 fi
+
+if [ -z "$WORDPRESS_ADMIN_USERNAME" ] || [ -z "$WORDPRESS_ADMIN_PASSWORD" ]; then
+	echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
+	exit 1
+fi
+
+set -ex
 
 ###
 # Create a new environment for this particular test run.
