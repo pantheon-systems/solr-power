8364ed99910e533260a7d11aeae3cf42512a4968
Ritesh-patel <ritzpatel91@gmail.com>
2016-10-03 23:21:00 +0530

    Update Copyright in plugin main file

--- solr-power.php
+++ solr-power.php
@@ -9,7 +9,7 @@
   Text Domain: solr-for-wordpress-on-pantheon
  */
 /*
-  Copyright (c) 2011 Matt Weber
+  Copyright (c) 2011 Pantheon
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
