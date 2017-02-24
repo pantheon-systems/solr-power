cd63a1bd789ad2278b6dd1ec555078c5f97229b7
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-23 16:40:18 -0800

    Remove old `wp solr index` code

--- includes/class-solrpower-cli.php
+++ includes/class-solrpower-cli.php
@@ -120,68 +120,6 @@ class SolrPower_CLI extends WP_CLI_Command {
 	}
 
 	/**
-	 * Index all posts for a site.
-	 *
-	 * [--posts_per_page=<count>]
-	 * : Index a specific number of posts per set.
-	 * ---
-	 * default: 300
-	 * ---
-	 *
-	 * [--post_type=<type>]
-	 * : Limit indexing to a specific post type.
-	 */
-	public function index_old( $args, $assoc_args ) {
-		$defaults = array(
-			'posts_per_page' => 300,
-			'post_status'	 => 'publish',
-			'fields'		 => 'ids',
-			'paged'			 => 1,
-			'post_type'		 => get_post_types( array( 'exclude_from_search' => false ) ),
-		);
-		// Check if specified post_type is valid.
-		if ( isset( $assoc_args[ 'post_type' ] ) && (false === post_type_exists( $assoc_args[ 'post_type' ] )) ) {
-			WP_CLI::error( '"' . $assoc_args[ 'post_type' ] . '" is an invalid post type.' );
-		}
-		$query_args		 = array_merge( $defaults, $assoc_args );
-		$query			 = new WP_Query( $query_args );
-		$current_page	 = $query->get( 'paged' );
-		$total			 = $query->max_num_pages;
-		// There's a bug with found_posts that shows two more than what it should.
-		$total_posts	 = (1 == $query->max_num_pages) ? $query->post_count : $query->found_posts - 2;
-
-		$notify	 = \WP_CLI\Utils\make_progress_bar( 'Indexing Items:', $total_posts );
-		$done	 = 0;
-		$failed	 = 0;
-		$solr	 = get_solr();
-		$update	 = $solr->createUpdate();
-		while ( $current_page <= $total ) {
-			$query->set( 'paged', $current_page );
-			$query->get_posts();
-			foreach ( $query->posts as $id ) {
-				$documents	 = array();
-				$documents[] = SolrPower_Sync::get_instance()->build_document( $update->createDocument(), get_post( $id ) );
-				$post_it	 = SolrPower_Sync::get_instance()->post( $documents, true, FALSE );
-
-				if ( false === $post_it ) {
-					$failed++;
-				} else {
-					$done++;
-				}
-				$notify->tick();
-			}
-			$current_page++;
-		}
-		do_action( 'solr_power_index_all_finished' );
-		$notify->finish();
-		WP_CLI::success( sprintf( '%d of %d items indexed.', $done, $total_posts ) );
-		if ( 0 < $failed ) {
-			WP_CLI::error( 'Failed to index ' . $failed . ' item(s).' );
-			WP_CLI::error( SolrPower_Sync::get_instance()->error_msg );
-		}
-	}
-
-	/**
 	 * Report information about Solr Power configuration.
 	 *
 	 * Displays ping status, alongside Solr server IP address, port, and path.
