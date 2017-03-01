9544691f16e6c9857e626e61cafd751f50113171
Andrew Taylor <ataylorme@users.noreply.github.com>
2017-03-01 11:10:28 -0600

    Disallow Solr in WordPress admin unless coming from admin ajax (#216)
    * Disallow Solr in WordPress admin unless coming from admin ajax
    * Closes #215

--- includes/class-solrpower-wp-query.php
+++ includes/class-solrpower-wp-query.php
@@ -70,7 +70,7 @@ class SolrPower_WP_Query {
 			 * @param bool $solr_allow_ajax True to query on AJAX or false [default false].
 			 */
 			if ( false === apply_filters( 'solr_allow_ajax', false ) ) {
-				// return;
+				return;
 			}
 		}
 
@@ -81,8 +81,8 @@ class SolrPower_WP_Query {
 		 *
 		 * @param bool $solr_allow_admin True to query in WordPress Dashboard or false [default false].
 		 */
-		if ( is_admin() && false === apply_filters( 'solr_allow_admin', false ) ) {
-			// return;
+		if ( is_admin() && false === apply_filters( 'solr_allow_admin', false ) && ( ! ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) ) {
+			return;
 		}
 
 
