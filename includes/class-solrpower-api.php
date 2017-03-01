c2ada9a1da76d049fc61e072ccb759c1fc06a971
ataylorme <andrew@ataylor.me>
2017-03-01 15:02:54 -0600

    Try setBoostQueries instead of setPhraseFields

--- includes/class-solrpower-api.php
+++ includes/class-solrpower-api.php
@@ -347,7 +347,8 @@ class SolrPower_Api {
 
 			$solr_boost_query = apply_filters( 'solr_boost_query', 'post_title^25 post_content^50' );
 			if ( false !== $solr_boost_query ) {
-				$dismax->setPhraseFields( $solr_boost_query );
+				$dismax->setBoostQueries( array( $solr_boost_query ) );
+				// $dismax->setPhraseFields( $solr_boost_query );
 				// $dismax->setQueryFields( $solr_boost_query );
 				// $dismax->setBoostFunctions( $solr_boost_query );
 			}
