ffcf0fa10fae6839af95dc8436b5db80f87a3564
Daniel Bachhuber <daniel@handbuilt.co>
2017-04-17 13:14:23 -0700

    Respect all query vars when searching with WP_Query (#246)
    Respect all query vars when searching with WP_Query
    
    The prior behavior would bail early when `s` was set but
    `solr_integrate` wasn't provided, which meant query vars would be
    discarded when searching
    
    Closes #244

--- tests/phpunit/wp_query/test-wp-query.php
+++ tests/phpunit/wp_query/test-wp-query.php
@@ -160,4 +160,18 @@ class SolrWPQueryTest extends SolrTestBase {
 		wp_reset_postdata();
 		SolrPower_Api::get_instance()->ping=true;
 	}
+
+	public function test_wp_query_search_filter_post_type() {
+		$post_id = $this->__create_test_post(); // Title defaults to 'Test Post'
+		$page_id = $this->__create_test_post( 'page' ); // Title defaults to 'Test Post'
+		$args = array(
+			's'         => 'Test',
+			'post_type' => 'page',
+		);
+		$query = new WP_Query( $args );
+		$this->assertEquals( 1, $query->post_count );
+		$this->assertEquals( 1, $query->found_posts );
+		$this->assertEquals( 'page', $query->get('post_type') );
+		$this->assertEquals( array( $page_id ), wp_list_pluck( $query->posts, 'ID' ) );
+	}
 }
