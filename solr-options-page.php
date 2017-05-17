14ad055611f476d98698a22200ddbbf9bab88398
Daniel Bachhuber <daniel@handbuilt.co>
2017-05-17 16:55:52 -0700

    Include original plugin attribution in the copyright notice

--- solr-options-page.php
+++ solr-options-page.php
@@ -4,28 +4,6 @@
  *
  * @package Solr_Power
  */
-
-/*
-	Copyright (c) 2009 Matt Weber
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-	THE SOFTWARE.
-*/
 ?>
 <div id="solr_header">
 	<h2>Solr Power <span>Version <?php echo esc_html( SOLR_POWER_VERSION ); ?></span></h2>
