b7acbc18869b617b8336540861dca881eba38286
Andrew Taylor <ataylorme@users.noreply.github.com>
2017-06-29 11:28:35 -0500

    Version 1.3.0 (#297)
    * Regenerate .pot file
    
    * Update README for 1.3.0
    
    * Bump plugin version to 1.3.0
    
    * Bump version to 1.3.0
    
    * Add package-lock.json for 1.3.0
    
    * Update the SOLR_POWER_VERSION constant too

--- solr-power.php
+++ solr-power.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Solr Power
  * Description: Allows WordPress sites to index and search content with ApacheSolr.
- * Version: 1.2.0
+ * Version: 1.3.0
  * Author: Pantheon
  * Author URI: http://pantheon.io
  * Text Domain: solr-for-wordpress-on-pantheon
@@ -10,7 +10,7 @@
  * @package Solr_Power
  **/
 
-define( 'SOLR_POWER_VERSION', '1.2.0' );
+define( 'SOLR_POWER_VERSION', '1.3.0' );
 
 /*
   Copyright (c) 2011-2017 Pantheon, Matt Weber, Solr Power contributors
