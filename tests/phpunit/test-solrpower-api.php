917a58fcc0981033a4a6d5c829377043623d389b
Pavel Lishin <pavel.lishin@gmail.com>
2019-05-30 18:13:53 -0400

    #408: Adding test

---
+++ tests/phpunit/test-solrpower-api.php
@@ -0,0 +1,40 @@
+<?php
+
+class SolrPowerAPITest extends SolrTestBase {
+
+	function setUp() {
+		parent::setUp();
+	}
+
+	function tearDown() {
+		parent::tearDown();
+	}
+
+
+	function test_server_info_uses_actual_config() {
+		$env_host      = $ENV['PANTHEON_INDEX_HOST'];
+		$env_port      = $ENV['PANTHEON_INDEX_HOST'];
+		$override_host = $env_host . 'OVERRIDE';
+		$override_port = $env_host . 'OVERRIDE';
+
+		$override_connection = function() {
+			return array(
+				'endpoint' => array(
+					'localhost' => array(
+						'host' => $override_host,
+						'port' => $override_port,
+						'path' => '',
+					)
+				),
+			);
+		};
+		add_filter( 's4wp_connection_options', $override_connection );
+
+		$server_info = SolrPower_Api::get_instance()->get_server_info();
+
+		$this->assertEquals( $override_host, $server_info['ip_address'] );
+		$this->assertEquals( $override_port, $server_info['port'] );
+
+		remove_filter( 's4wp_connection_options', $override_connection );
+	}
+}
