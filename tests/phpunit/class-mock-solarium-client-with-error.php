d3e67577a2604b59faec4074489257a18778d719
John Spellman <john.spellman@pantheon.io>
2023-03-21 09:56:25 -0600

    Fix test error

--- tests/phpunit/class-mock-solarium-client-with-error.php
+++ tests/phpunit/class-mock-solarium-client-with-error.php
@@ -1,9 +1,12 @@
 <?php
 
+use Solarium\QueryType\Update\Result as UpdateResult;
+
 class Mock_Solarium_Client_With_Error extends Solarium\Client {
 	public static $error_msg = "";
 
-	public function update(Solarium\Core\Query\QueryInterface $update, $endpoint = null) {
+	public function update(Solarium\Core\Query\QueryInterface $update, $endpoint = null) : UpdateResult
+	{
 		throw new Exception(self::$error_msg);
 	}
 }
