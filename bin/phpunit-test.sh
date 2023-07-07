54fe226e3a9e4b7c1c9f0385d73efee908087fd3
Phil Tyler <phil.tyler@pantheon.io>
2023-07-07 10:41:27 -0700

    phpunit test to standalone shell

---
+++ bin/phpunit-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+DIRNAME=$(dirname "$0")
+
+bash bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest
+# composer phpunit
+# rm -rf $WP_TESTS_DIR $WP_CORE_DIR
+
+composer phpunit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.0$/" || true
+composer phpunit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.1$/" || true
+composer phpunit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.2$/" || true
+rm -rf $WP_TESTS_DIR $WP_CORE_DIR
+
+composer phpunit-shouldcommit
+rm -rf $WP_TESTS_DIR $WP_CORE_DIR
+bash bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest true
+WP_MULTISITE=1 composer phpunit
+rm -rf $WP_TESTS_DIR $WP_CORE_DIR
+WP_MULTISITE=1 composer phpunit-shouldcommit
