991f402fc4e50c0419f10fddfe799648a62ed211
Phil Tyler <phil.tyler@pantheon.io>
2023-07-07 10:45:14 -0700

    group flag

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -7,9 +7,9 @@ bash bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest
 # composer phpunit
 # rm -rf $WP_TESTS_DIR $WP_CORE_DIR
 
-composer phpunit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.0$/" || true
-composer phpunit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.1$/" || true
-composer phpunit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.2$/" || true
+composer phpunit --group should-commit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.0$/" || true
+composer phpunit --group should-commit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.1$/" || true
+composer phpunit --group should-commit --filter testShouldCommit::shouldCommitTestValues --filter "/@shouldCommitTestValues\.2$/" || true
 rm -rf $WP_TESTS_DIR $WP_CORE_DIR
 
 composer phpunit-shouldcommit
