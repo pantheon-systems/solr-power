1522cf0084c5d452439f4bde6cde2f36acc48a51
Phil Tyler <phil.tyler@pantheon.io>
2023-07-07 12:51:50 -0700

    run thru composer?

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -3,14 +3,14 @@ set -e
 
 DIRNAME=$(dirname "$0")
 bash bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest 
-composer phpunit
+composer test
 
 # PHPUnit was deeply unhappy with setting the constant as part of the tests,
 # so these are run separately.
-${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldNotCommitWhenConstNull
-${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldNotCommitWhenConstTrue
-${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldCommitWhenConstFalse
+# ${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldNotCommitWhenConstNull
+# ${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldNotCommitWhenConstTrue
+# ${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldCommitWhenConstFalse
 rm -rf $WP_TESTS_DIR $WP_CORE_DIR
 
 bash bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest true
-WP_MULTISITE=1 composer phpunit
+WP_MULTISITE=1 composer test
