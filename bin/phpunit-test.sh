e760f5dd869a32bdf9121f7ecb15681094b410c2
Phil Tyler <phil.tyler@pantheon.io>
2023-07-07 13:10:29 -0700

    cleanup phpunit test script

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -2,15 +2,9 @@
 set -e
 
 DIRNAME=$(dirname "$0")
-bash bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest 
+bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
 composer test
-
-# PHPUnit was deeply unhappy with setting the constant as part of the tests,
-# so these are run separately.
-# ${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldNotCommitWhenConstNull
-# ${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldNotCommitWhenConstTrue
-# ${DIRNAME}/../vendor/bin/phpunit --group should-commit --filter testShouldCommitWhenConstFalse
 rm -rf $WP_TESTS_DIR $WP_CORE_DIR
 
-bash bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest true
+bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest true
 WP_MULTISITE=1 composer test
