6a8e404f1bce3292237e5249d94e51aa2d187c75
Phil Tyler <phil.tyler@pantheon.io>
2024-06-17 08:58:37 -0700

    Prepare 2.5.4-dev

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: search
 Requires at least: 4.6
 Requires PHP: 7.1
 Tested up to: 6.5.2
-Stable tag: 2.5.3
+Stable tag: 2.5.4-dev
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- solr-power.php
+++ solr-power.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Solr Power
  * Description: Allows WordPress sites to index and search content with ApacheSolr.
- * Version: 2.5.3
+ * Version: 2.5.4-dev
  * Author: Pantheon
  * Author URI: http://pantheon.io
  * Text Domain: solr-for-wordpress-on-pantheon
@@ -10,7 +10,7 @@
  * @package Solr_Power
  **/
 
-define( 'SOLR_POWER_VERSION', '2.5.3' );
+define( 'SOLR_POWER_VERSION', '2.5.4-dev' );
 
 /**
  * Copyright (c) 2011-2022 Pantheon, Matt Weber, Solr Power contributors
