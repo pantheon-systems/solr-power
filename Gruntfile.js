6f1d90aa84a33c02e5875e2105fcb76df88db735
Phil Tyler <phil.tyler@pantheon.io>
2024-12-04 10:19:34 -0800

    Replace deprecated node-sass package

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function( grunt ) {
 	'use strict';
-	const sass = require( 'node-sass' );
+	const sass = require( 'sass' );
 	// Project configuration
 	grunt.initConfig( {
 		pkg:    grunt.file.readJSON( 'package.json' ),
