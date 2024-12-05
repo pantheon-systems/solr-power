32570ca67b716afa332cca60d485f98796cc1f8e
Phil Tyler <phil.tyler@pantheon.io>
2024-12-05 12:29:25 -0800

    rm wrong uploads test csae

--- tests/phpunit/test-solr.php
+++ tests/phpunit/test-solr.php
@@ -593,22 +593,14 @@ EOT;
 		$this->assertContains($expected, $actual);
 	}
 
-	public function test_custom_schema_file_path_wp_content_moved() {
-		define( 'UPLOADS', trailingslashit( WP_CONTENT_DIR ) . 'files' );
-		$expected = "wp-content/files/solr-for-wordpress-on-pantheon/schema.xml";
-		$actual = SolrPower_Api::custom_schema_file_path();
-
-		$this->assertContains($expected, $actual);
-	}
-
 	public function test_custom_schema_file_path_with_filter() {
-		$expected = "/code/wp-content/solr-for-wordpress-elsewhere-on-pantheon/schema.xml";
 		add_filter(
 			'solr_power_customer_schema_file_path',
 			function( $custom_schema_file_path ) {
-				return $expected;
+				return "/code/wp-content/solr-for-wordpress-elsewhere-on-pantheon/schema.xml";
 			},
 		);
+		$expected = "/code/wp-content/solr-for-wordpress-elsewhere-on-pantheon/schema.xml";
 		$actual = SolrPower_Api::custom_schema_file_path();
 
 		$this->assertEquals($expected, $actual);
